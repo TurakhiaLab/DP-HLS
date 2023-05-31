19af252b135c98916494eb4d047d22f6b4baf95d
Yingqi Cao <yic033@ucsd.edu>
2023-05-30 21:56:53 -0700

    reconstruct vitis template repository

---
+++ templates/vitis/project_config.tcl
@@ -0,0 +1,12 @@
+open_project {{ project_name }}
+set_top {{ top_level_function }}
+add_files {{ project_files }}
+add_files -tb {{ testbench_files }}
+open_solution "{{ solution_name }}" -flow_target vivado
+set_part { {{ device }} }
+create_clock -period {{ clock_period }} -name default
+
+csim_design -clean -setup
+csynth_design
+cosim_design
+export_design -format ip_catalog
---
+++ templates/vitis/vitis_project.sh
@@ -0,0 +1,2 @@
+source {{ path_settings64 }};
+vitis_hls -f {{ create_project_path }}
