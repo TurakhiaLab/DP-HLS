b6e0ebd35e741ae8adb8062416c12debe99d50e4
Yingqi Cao <yic033@ucsd.edu>
2024-07-29 07:00:17 +0000

    cleaned repository

--- module.cpp
+++ module.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file module.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "./include/pyapi.h"
